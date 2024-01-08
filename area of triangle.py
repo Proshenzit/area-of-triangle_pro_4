0194c0fd8569cfe640b95d1a98f597610f39e19c
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-01-07 21:45:10 -0800

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,6 @@
+a  = int(input("enter your  first number: "))
+b  = int(input("enter your  first number: "))
+c  = int(input("enter your  first number: "))
+s = (a+b+c)/2
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+print("The area  of the  triangle %0.2f: " %area)
